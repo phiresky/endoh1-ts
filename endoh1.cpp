c8dffc6970f9f2ab899a8ac694ef9d7c351938b6
phiresky <phiresky@users.noreply.github.com>
2015-05-20 21:10:41 +0200

    localize variables, restructure loops

--- endoh1.cpp
+++ endoh1.cpp
@@ -29,75 +29,71 @@ struct complex {
 		y += p.y;
 		return *this;
 	}
-	explicit operator double() {
-		return x;
-	}
 	double x,y;
-};
+	double abs() {
+		return sqrt(x*x+y*y);
+	}
 
-complex I = {0,1};
+};
 
 complex operator -(double p, complex q) {
 	return complex{p} - q;
 }
 
-double cabs(complex p) {
-	return sqrt(p.x*p.x+p.y*p.y);
-}
+complex a[97687];
+char b[6856] = "\x1b[2J\x1b[1;1H"; // clear screen, go to pos 1,1
 
-double creal(complex p) { return p.x;}
-complex a[97687], w = 0, d;
-int p, q, r = 0;
-int x, y;
-char b[6856] = "\x1b[2J" "\x1b" "[1;1H     ";
-int o = 0, t;
-
-
-
-int main() {
-
-	while (0 < (x = getc(stdin))) {
+int init() {
+	int x,r = 0;
+	complex w;
+	while ((x = getchar()) != EOF) {
 		if (x > 10) {
 			if (32 < x) {
 				a[r++] = w;
 				a[r + 4] = w + 1;
-				a[r] = a[r + 5] = x == 35;
+				a[r] = a[r + 5] = x == '#';
 				r += 9;
 			}
-			w = w - I;
+			w.y--;
 		} else {
-			w = (int) creal(w) + 2;
+			w.x += 2;
+			w.y = 0;
 		}
 	}
-	while (1) {
-		for (p = 0; p < r; p += 5) {
+	return r;
+}
+
+int main() {
+	int r = init();
+	puts(b);
+	while(true) {
+		for (int p = 0; p < r; p += 5) {
 			a[p + 2] = a[p + 1] * 9;
-			for (q = 0; q < r; q += 5) {
-				d = a[p] - a[q];
-				w = cabs(d) / 2 - 1;
-				if (0 < (x = double(1 - w)))
+			for (int q = 0; q < r; q += 5) {
+				double w = (a[p] - a[q]).abs() / 2 - 1;
+				if (0 < int(1 - w))
 					a[p + 2] += w * w;
 			}
 		}
-		for (p = 0; p < r; p += 5) {
+		for (int p = 0; p < r; p += 5) {
 			a[p + 3] = G;
-			for (q = 0; q < r; q += 5) {
-				d = a[p] - a[q];
-				w = cabs(d) / 2 - 1;
-				x = double(1 - w);
-				if (x > 0)
-					a[p + 3] += w
-							* (d * (3 - a[p + 2] - a[q + 2]) * P + a[p + 4] * V
-									- a[q + 4] * V) / a[p + 2];
+			for (int q = 0; q < r; q += 5) {
+				complex d = a[p] - a[q];
+				double w = d.abs() / 2 - 1;
+				if (int(1-w) > 0)
+					a[p + 3] += (d * (3 - a[p + 2] - a[q + 2]) * P + a[p + 4] * V
+									- a[q + 4] * V)*w/ a[p + 2];
 			}
 		}
-		for (x = 011; 2012 - 1 > x++;)
+		for (int x = 10; x<2011; x++)
 			b[x] = 0;
-		for (p = 0; p < r; p += 5) {
-			t = 10 + (x = double(a[p] * I)) + 80 * (y = double(a[p] / 2));
-			a[p] += a[p + 4] += a[p + 3] / 10 * !double(a[p + 1]);
-			x = 0 <= x && x < 79 && 0 <= y && y < 23;
-			if (x) {
+		for (int p = 0; p < r; p += 5) {
+			int x = -a[p].y;
+			int y = a[p].x / 2;
+			int t = 10 + x + 80 * y;
+			a[p] += a[p + 4] += a[p + 3] / 10 * !(a[p + 1]).x;
+			if (0 <= x && x < 79 && 0 <= y && y < 23) {
+				// on screen
 				b[t] |= 8;
 				b[t + 1] |= 4;
 				t += 80;
@@ -105,73 +101,14 @@ int main() {
 				b[t] |= 2;
 			}
 		}
-		for (x = 011; 2012 - 1 > x++;)
-			b[x] = " '`-.|//,\\" "|\\_" "\\/\x23\n"[x % 80 - 9 ? x[b] : 16];
+		for (int x = 10; x<2011; x++) {
+			if(x % 80 - 9)
+				b[x] = " '`-.|//,\\|\\_\\/#\n"[int(b[x])];
+			else b[x] = '\n';
+		}
 
 		usleep(12321);
-		puts(b + o);
-		o = 4;
+		puts(b + 4);
 	}
 	return 0;
 }
-/*
-int main() {
-
-	while (0 < (x = getc(stdin))) {
-		if (x > 10) {
-			if (32 < x) {
-				a[r++] = w;
-				a[r + 4] = w + 1;
-				a[r] = a[r + 5] = x == 35;
-				r += 9;
-			}
-			w.y -= 1;
-		} else {
-			w = (int) w.x + 2;
-		}
-	}
-	while (1) {
-		for (p = 0; p < r; p += 5) {
-			a[p + 2] = a[p + 1] * 9;
-			for (q = 0; q < r; q += 5) {
-				d = a[p] - a[q];
-				w = cabs(d) / 2 - 1;
-				if (0 < (x = 1 - w.x))
-					a[p + 2] += w * w;
-			}
-		}
-		for (p = 0; p < r; p += 5) {
-			a[p + 3] = G;
-			for (q = 0; q < r; q += 5) {
-				d = a[p] - a[q];
-				w = cabs(d) / 2 - 1;
-				x = 1 - w.x;
-				if (x > 0)
-					a[p + 3] += w
-							* (d * (complex{3,0} - a[p + 2] - a[q + 2]) * P + a[p + 4] * V
-									- a[q + 4] * V) / a[p + 2];
-			}
-		}
-		for (x = 011; 2012 - 1 > x++;)
-			b[x] = 0;
-		for (p = 0; p < r; p += 5) {
-			t = 10 + (x = (a[p] * I).x) + 80 * (y = (a[p] / 2).x);
-			a[p] += a[p + 4] += a[p + 3] / 10 * !(a[p + 1].x);
-			x = 0 <= x && x < 79 && 0 <= y && y < 23;
-			if (x) {
-				b[t] |= 8;
-				b[t + 1] |= 4;
-				t += 80;
-				b[t + 1] = 1;
-				b[t] |= 2;
-			}
-		}
-		for (x = 011; 2012 - 1 > x++;)
-			b[x] = " '`-.|//,\\" "|\\_" "\\/\x23\n"[x % 80 - 9 ? x[b] : 16];
-
-		usleep(12321);
-		puts(b + o);
-		o = 4;
-	}
-	return 0;
-}*/
