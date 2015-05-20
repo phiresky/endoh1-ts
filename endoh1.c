86a485d4343a37dd793c4750eb553b38d8b68323
phiresky <phiresky@users.noreply.github.com>
2015-05-20 21:05:48 +0200

    expand comma operators

--- endoh1.c
+++ endoh1.c
@@ -1,31 +1,46 @@
 #include <stdio.h>//  .IOCCC                                         Fluid-  #
 #include <unistd.h>  //2012                                         _Sim!_  #
 #include <complex.h>  //||||                     ,____.              IOCCC-  #
-double complex	a[97687], *p, *q, *r = a, w = 0, d;
-int		x         , y;
-char		b         [6856] = "\x1b[2J" "\x1b" "[1;1H     ", *o = b, *t;
-int
-main()
-{
+double complex a[97687], w = 0, d;
+int p, q, r = 0;
+int x, y;
+char b[6856] = "\x1b[2J" "\x1b" "[1;1H     ", *o = b, *t;
+int main() {
 
-	for (; 0 < (x = getc(stdin));)
-		w = x > 10 ? 32 < x ? 4[*r++ = w, r] = w + 1, *r = r[5] = x == 35, r += 9 : 0, w - I : (x = w + 2);;
+	for (; 0 < (x = getc(stdin));) {
+		if (x > 10) {
+			if (32 < x) {
+				a[r++] = w;
+				a[r + 4] = w + 1, a[r] = a[r + 5] = x == 35, r += 9;
+			}
+			w = w - I;
+		} else {
+			w = (x = w + 2);
+		}
+	}
 	for (;; puts(o), o = b + 4) {
-		for (p = a; p[2] = p[1] * 9, p < r; p += 5)
-			for (q = a; w = cabs(d = *p - *q) / 2 - 1, q < r; q += 5)
+		for (p = 0; a[p + 2] = a[p + 1] * 9, p < r; p += 5)
+			for (q = 0; w = cabs(d = a[p] - a[q]) / 2 - 1, q < r; q += 5)
 				if (0 < (x = 1 - w))
-					p[2] += w * w;
-		for (p = a; p[3] = G, p < r; p += 5)
-			for (q = a; w = cabs(d = *p - *q) / 2 - 1, q < r; q += 5)
+
+
+					a[p + 2] += w * w;
+		for (p = 0; a[p + 3] = G, p < r; p += 5)
+			for (q = 0; w = cabs(d = a[p] - a[q]) / 2 - 1, q < r; q += 5)
 				if (0 < (x = 1 - w))
-					p[3] += w * (d * (3 - p[2] - q[2]) * P + p[4] * V - q[4] * V) / p[2];
+					a[p + 3] += w
+							* (d * (3 - a[p + 2] - a[q + 2]) * P + a[p + 4] * V
+									- a[q + 4] * V) / a[p + 2];
 		for (x = 011; 2012 - 1 > x++;)
 			b[x] = 0;
-		for (p = a; (t = b + 10 + (x = *p * I) + 80 * (y = *p / 2), *p += p[4] += p[3] / 10 * !p[1]), p < r; p += 5)
-			x = 0 <= x && x < 79 && 0 <= y && y < 23 ? 1[1[*t |= 8, t] |= 4, t += 80] = 1, *t |= 2 : 0;
+		for (p = 0; (t = b + 10 + (x = a[p] * I) + 80 * (y = a[p] / 2), a[p] +=
+				a[p + 4] += a[p + 3] / 10 * !a[p + 1]), p < r; p += 5)
+			if (x = 0 <= x && x < 79 && 0 <= y && y < 23)
+				1[1[*t |= 8, t] |= 4, t += 80] = 1, *t |= 2;
 		for (x = 011; 2012 - 1 > x++;)
-			b[x] = " '`-.|//,\\" "|\\_" "\\/\x23\n"[x % 80 - 9 ? x[b] : 16];;
+			b[x] = " '`-.|//,\\" "|\\_" "\\/\x23\n"[x % 80 - 9 ? x[b] : 16];
 
 		usleep(12321);
-	} return 0;
+	}
+	return 0;
 }
